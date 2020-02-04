4e5d67f36026927f0beabd4f48c3f422a4690a92
James Kirtland <james@sourcelift.co>
2020-02-04 06:28:09 +0000

    Added status values in config, similar to Components.

--- config/laravel-status-page.php
+++ config/laravel-status-page.php
@@ -7,5 +7,12 @@ return [
         2 => 'Performance Issues',
         3 => 'Partial Outage',
         4 => 'Major Outage'
+    ],
+    'incident-statuses' => [
+        0 => 'Unknown',
+        1 => 'Investigating',
+        2 => 'Identified',
+        3 => 'Watched',
+        4 => 'Fixed'
     ]
 ];
