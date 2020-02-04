a6c9e0edda62c90f172bdb285954089b40073037
James Kirtland <james@sourcelift.co>
2020-02-04 07:04:57 +0000

    Starting routing and controller for restful incident api.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,27 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => ['auth:api']], function() {
+
+    /**
+     * Incident RESTful routing.
+     */
+
+    /**
+     * Create incident.
+     */
+    Route::post('incident/create', 'IncidentController@create');
+
+    /**
+     * Read incident details.
+     */
+    Route::get('incident/{id}', 'IncidentController@show');
+
+    /**
+     * Add update to incident.
+     */
+    Route::get('incident/{id}/update', 'IncidentControllers@addUpdate');
+});
+
